d68500f6f77baf976fe66d37f08e7304d5d98d1f
harryspackman <hspack@hotmail.co.uk>
2020-11-06 14:12:25 +0000

    Updating to  commit version where code updates position of two objects using the gravitational force between them

--- SolarSystem/Header.h
+++ SolarSystem/Header.h
@@ -1,26 +1,35 @@
 #pragma once
 #include <vector>
+#include "functions.h"
+
 
 using namespace std;
 
+
 //declaring class of type Body, all members are public
+//taken verlet integrals and vectors from "https://en.wikipedia.org/wiki/Verlet_integration"
 class Body {
 
 public:
-	int mass;
-	float r, theta;
+	double mass;
+	double r, theta;
 
-	vector <double> pos {0, 0, 0};
-	vector <double> vel {0, 0, 0};
-	vector <double> acc {0, 0, 0};
+	double pos[3];
+	double vel[3];
+	double acc[3];
+	double new_pos[3];
+	double new_vel[3];
+	double new_acc[3];
 
-	Body(int, float, float);
+	
+	Body(double, double, double);
 
+	
 };
 
 
 //constuctor function for body
-Body::Body(int a, float b, float c) {
+Body::Body(double a, double b, double c) {
 	mass = a;
 	r = b;
 	theta = c;
--- SolarSystem/SolarSystem.cpp
+++ SolarSystem/SolarSystem.cpp
@@ -1,6 +1,7 @@
 
 #include "pch.h"
 #include <iostream>
+#include <math.h>
 #include "header.h"
 
 
@@ -8,32 +9,106 @@
 
 using namespace std;
 
+//function declarations
+void calculate_acceleration(int dt, Body &body1, Body &body2);
+
+void update_position(int dt, Body &body1, Body &body2);
+
+void update_velocity(int dt, Body &body1, Body &body2);
+
+
 int main()
 {
 
 	//initialising objects
-	Body sun(100000000, 0, 0);
-	Body earth(1, 100, 0);
+	Body sun(1.989e30, 0, 0);
+	Body earth(5.972e24, 100, 0);
+
+	
+	//initialising initial positions, velocities and accelerations of two bodies
+	sun.pos[0] = 5;
+	sun.pos[1] = 5;
+	sun.pos[2] = 5;
+
+	sun.vel[0] = 0.1;
+	sun.vel[1] = 0.1;
+	sun.vel[2] = 0.2;
+	
+	sun.acc[0] = 0.02;
+	sun.acc[1] = 0.02;
+	sun.acc[2] = 0.03;
+
+	earth.pos[0] = 100e5;
+	earth.pos[1] = 100e5;
+	earth.pos[2] = 100e5;
+
+	earth.vel[0] = 0.5;
+	earth.vel[1] = 0.8;
+	earth.vel[2] = 0.9;
+
+	earth.acc[0] = 0.01;
+	earth.acc[1] = 0.02;
+	earth.acc[2] = 0.03;
+
+	for (int dt = 0; dt < 5; dt++) {
+		calculate_acceleration(dt, sun, earth);
+		update_position(dt, sun, earth);
+		update_velocity(dt, sun, earth);
+	}
 
 
-	//calculating orbital velocity
-	float v = sqrt((G * sun.mass) / earth.r);
+}
 
 
-	//calculating time period of orbit
-	float cap_t = 2 * 3.1415 * earth.r / v;
+//passing into function time increment, and the two objects by reference
+void calculate_acceleration(int dt, Body &body1, Body &body2) {
 
+	double squar[3];
 
-	//looping and calculating angular position of earth as a function of time in seconds printing time and angular position to terminal
-	for (int t = 0; t < cap_t; t += 10) 
-	{
+	//calculating distance between body1 and body2
+	for (int i = 0; i < 3; i++) {
+		squar[i] = pow((body1.pos[i] - body2.pos[i]), 2);
 
-		//angular position in radians
-		earth.theta = v * t / earth.r;
+	}
+	
+	double r = sqrt(squar[0] + squar[1] + squar[2]);
 
-		cout << t << '\t' << earth.theta << endl;
+	//calculating gravitational accelerations of bodies for each direction
+	for (int j = 0; j < 3; j++) {
+
+		body1.new_acc[j] = G * body2.mass * (body1.pos[j] - body2.pos[j]) / pow(r, 3);
+		body2.new_acc[j] = G * body1.mass * (body2.pos[j] - body1.pos[j]) / pow(r, 3);
+		
+	}
+
+	
+}
+
+//calculates new position
+void update_position(int dt, Body &body1, Body &body2) {
+
+	for (int i = 0; i < 3; i++) {
+		body1.new_pos[i] = body1.pos[i] + body1.vel[i] * dt + body1.acc[i] * (dt*dt*0.5);
+		body1.pos[i] = body1.new_pos[i];
+
+		body2.new_pos[i] = body2.pos[i] + body2.vel[i] * dt + body2.acc[i] * (dt*dt*0.5);
+		body2.pos[i] = body2.new_pos[i];
 
 	}
 
+	printf("%f %f %f\n", body1.pos[0], body1.pos[1], body1.pos[2]);
+	printf("%f %f %f\n", body2.pos[0], body2.pos[1], body2.pos[2]);
+}
+
+//calculates new velocity
+void update_velocity(int dt, Body &body1, Body &body2) {
+
+	for (int i = 0; i < 3; i++) {
+		body1.new_vel[i] = body1.vel[i] + (body1.acc[i] + body1.new_acc[i])*(dt*0.5);
+		body1.vel[i] = body1.new_vel[i];
+
+		body2.new_vel[i] = body2.vel[i] + (body2.acc[i] + body2.new_acc[i])*(dt*0.5);
+		body2.vel[i] = body2.new_vel[i];
+	}
 
 }
